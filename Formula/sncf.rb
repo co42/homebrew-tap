5f7e5b2ef997e098b597a30b9be8368f5c45fff5
Corentin REGAL <corentin@huggingface.co>
2026-03-10 15:05:28 +0100

    sncf 1.1.0

--- Formula/sncf.rb
+++ Formula/sncf.rb
@@ -1,22 +1,22 @@
 class Sncf < Formula
   desc "SNCF train schedule CLI"
   homepage "https://github.com/co42/sncf"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/co42/sncf/releases/download/v1.0.0/sncf-v1.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ec83df33824255cb2a850a8a16330e66dcb817b8e4cc700350b5a6cf0f4186ba"
+      url "https://github.com/co42/sncf/releases/download/v1.1.0/sncf-v1.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "247d0058922b95ba75a6aa4a480e57445838cb08eb67106873578b8e9d3c310c"
     else
-      url "https://github.com/co42/sncf/releases/download/v1.0.0/sncf-v1.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "08d78fac581dbcc79dfb13e5936c1da78e60c386ab11016edabb84fcc5354d27"
+      url "https://github.com/co42/sncf/releases/download/v1.1.0/sncf-v1.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "cbda9161cf7f0120c4c20d97b3e69663f829d34ebef1980749c9862036ebd5cc"
     end
   end
 
   on_linux do
-    url "https://github.com/co42/sncf/releases/download/v1.0.0/sncf-v1.0.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "53dffbc07f398bd46f345acfb05692d997d8c80c0c093e31574bd3a7df1c443b"
+    url "https://github.com/co42/sncf/releases/download/v1.1.0/sncf-v1.1.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "9a12a8cf9473aad278053d12e31930e26a6e4f5471c5710fc27278c2c6b3ece3"
   end
 
   def install
